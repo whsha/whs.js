25a4abbc38c9f4657ffe18e0b1de6962e96b3884
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-08-22 13:06:53 -0400

    more fixes

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,7 +1,7 @@
 echo Logging into Expo
 $(yarn global bin)/expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
 
-CHANNEL=$([ $IS_STABLE == 'true' ] && echo "stable" || echo "staging")
+CHANNEL=$([ $IS_STABLE ] && echo "stable" || echo "staging")
 
 echo Publishing to $CHANNEL
 $(yarn global bin)/expo publish --non-interactive --release-channel $(echo $CHANNEL-$VERSION)
