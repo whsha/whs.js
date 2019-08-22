e613ccde003f0588bbf95a5d0d5ee4d28ef7ddca
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-08-22 13:35:41 -0400

    Fix env

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -2,12 +2,13 @@ echo Logging into Expo
 $(yarn global bin)/expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
 
 CHANNEL=$([ $IS_STABLE ] && echo "stable" || echo "staging")
+VERSION=$(sentry-cli releases propose-version)
 
 echo $CHANNEL $VERSION
 
 echo Publishing to $CHANNEL
-echo $(yarn global bin)/expo publish --non-interactive --release-channel $(echo $CHANNEL-$VERSION)
+$(yarn global bin)/expo publish --non-interactive --release-channel $(echo $CHANNEL-$VERSION)
 
 echo Creating a new sentry release
-echo sentry-cli releases new -p whs.js $VERSION
-echo sentry-cli releases set-commits --auto $VERSION
+sentry-cli releases new -p whs.js $VERSION
+sentry-cli releases set-commits --auto $VERSION
