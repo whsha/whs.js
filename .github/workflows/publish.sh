cdda31a423acb25b845a4c77b566561282fc636e
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-08-22 12:52:23 -0400

    Finish publish

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,14 +1,11 @@
 echo Logging into Expo
 $(yarn global bin)/expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
 
-echo $IS_MASTER $IS_STABLE
+CHANNEL=$([ $IS_STABLE == 'true' ] && echo "stable" || echo "staging")
 
-# echo Publishing to staging
-# $(yarn global bin)/expo publish --non-interactive --release-channel staging-$VERSION
-# echo Publish to stable
-# $(yarn global bin)/expo publish --non-interactive --release-channel stable-$VERSION
+echo Publishing to $CHANNEL
+$(yarn global bin)/expo publish --non-interactive --release-channel $(echo $CHANNEL-$VERSION)
 
-
-# echo Creating a new sentry release
-# sentry-cli releases new -p whs.js $VERSION
-# sentry-cli releases set-commits --auto $VERSION
+echo Creating a new sentry release
+sentry-cli releases new -p whs.js $VERSION
+sentry-cli releases set-commits --auto $VERSION
