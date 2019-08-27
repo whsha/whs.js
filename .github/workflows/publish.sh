74fb327d0798c4f68e6edd2bbb929b42b259f9e6
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-08-27 16:45:42 -0400

    fix publish script

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,22 +1,16 @@
 echo Logging into Expo
 $(yarn global bin)/expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
 
-VERSION=$(sentry-cli releases propose-version)
 CHANNEL=$([ $IS_STABLE = "true" ] && echo "stable" || echo "staging")
-RELEASE_CHANNEL=$(echo $CHANNEL-$VERSION)
 
 echo Publishing to $CHANNEL
-$(yarn global bin)/expo publish --non-interactive --release-channel $RELEASE_CHANNEL
-
-echo Creating a new sentry release
-sentry-cli releases new -p whs $VERSION
-sentry-cli releases set-commits --auto $VERSION
+$(yarn global bin)/expo publish --non-interactive --release-channel $CHANNEL
 
 WEBHOOK_DATA="{
    \"embeds\": [{
        \"title\": \"New $CHANNEL version published\",
        \"description\": \"$COMMIT_MESSAGE\",
-       \"url\": \"https://exp.host/@dusterthefirst/WHS?release-channel=$RELEASE_CHANNEL\",
+       \"url\": \"https://exp.host/@dusterthefirst/WHS?release-channel=$CHANNEL\",
        \"color\": 32768,
        \"timestamp\": \"$(date -Iseconds -u)\",
        \"footer\": {
@@ -35,7 +29,7 @@ WEBHOOK_DATA="{
 
 echo $WEBHOOK_DATA
 
-curl -sS --header "Content-Type: application/json" \
+echo curl -sS --header "Content-Type: application/json" \
   --request POST \
   --data "$WEBHOOK_DATA" \
   $WEBHOOK_URL
