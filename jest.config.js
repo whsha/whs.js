e3e05fd7dfbcb5bbf6d6b29974011b20c459ae7a
Zachary Kohnen <14093962+DusterTheFirst@users.noreply.github.com>
2019-09-03 18:55:38 -0400

    Added coverage report for all files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    collectCoverageFrom: [
+        "**/*.{ts,tsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ]
 };
