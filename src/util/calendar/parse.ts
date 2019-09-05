f562186c31e2251f2ab9ec66cdfe634a92f84b30
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-09-05 09:51:58 -0400

    Re add TZOFF

--- src/util/calendar/parse.ts
+++ src/util/calendar/parse.ts
@@ -21,6 +21,8 @@ enum SchoolDayRegexMatch {
     Meta
 }
 
+const TZOFF = new Date().getTimezoneOffset();
+
 /** Parse an ICal */
 export default function parseCalendar(rawical: string): ICalendarInformation {
     // Parse the ICal into a JCal
@@ -73,7 +75,7 @@ export default function parseCalendar(rawical: string): ICalendarInformation {
                 // The description of the event
                 description: description === null ? undefined : description,
                 // The end date of the event, if any
-                end: end === null ? undefined : dayjs(end.toJSDate()).subtract(4, "minute").toISOString(),
+                end: end === null ? undefined : dayjs(end.toJSDate()).subtract(TZOFF, "minute").toISOString(),
                 // If there is no end date or the event lasts 24 hours, that makes the event all day
                 isAllDay: end === null || dayjs(end.toJSDate()).diff(date.toJSDate(), "day") === 1,
                 // The location of the event
@@ -81,12 +83,10 @@ export default function parseCalendar(rawical: string): ICalendarInformation {
                 // The summary (title) of the event
                 name: summary,
                 // The timestamp of the events
-                stamp: stamp === null ? undefined : dayjs(stamp.toJSDate()).subtract(4, "minute").toISOString(),
+                stamp: stamp === null ? undefined : dayjs(stamp.toJSDate()).subtract(TZOFF, "minute").toISOString(),
                 // The start date of the event
-                start: dayjs(date.toJSDate()).subtract(4, "minute").toISOString()
+                start: dayjs(date.toJSDate()).subtract(TZOFF, "minute").toISOString()
             });
-
-            console.log(end, end !== null ? end.toJSDate() : null);
 
             // FIXME:
             break;
