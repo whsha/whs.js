d4832ee8d280431232b851a21a41cdc902106ca7
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-11-26 10:46:32 -0500

    test: wrap clear classes cell in nav container

--- src/components/settings/ClearClassesCell.test.tsx
+++ src/components/settings/ClearClassesCell.test.tsx
@@ -2,6 +2,7 @@
  * Copyright (C) 2018-2019  Zachary Kohnen (DusterTheFirst)
  */
 
+import { NavigationNativeContainer } from "@react-navigation/native";
 import React from "react";
 import { render } from "react-native-testing-library";
 import ClearClassesCell from "./ClearClassesCell";
@@ -9,7 +10,7 @@ import ClearClassesCell from "./ClearClassesCell";
 // TODO: Test press
 
 it("Renders <ClearClassesCell/>", () => {
-    const comp = render(<ClearClassesCell/>);
+    const comp = render(<NavigationNativeContainer><ClearClassesCell/></NavigationNativeContainer>);
 
     expect(comp.toJSON()).toMatchSnapshot();
 });
