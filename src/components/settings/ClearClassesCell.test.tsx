a690cfa80e9d29c44b926a5b0a5ca29f4f34b503
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-11-26 10:55:55 -0500

    test: fix clear class test

--- src/components/settings/ClearClassesCell.test.tsx
+++ src/components/settings/ClearClassesCell.test.tsx
@@ -3,14 +3,24 @@
  */
 
 import { NavigationNativeContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import { render } from "react-native-testing-library";
 import ClearClassesCell from "./ClearClassesCell";
 
 // TODO: Test press
 
+/** Test navigator */
+const Stack = createStackNavigator();
+
 it("Renders <ClearClassesCell/>", () => {
-    const comp = render(<NavigationNativeContainer><ClearClassesCell/></NavigationNativeContainer>);
+    const comp = render(
+        <NavigationNativeContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Test" component={ClearClassesCell}/>
+            </Stack.Navigator>
+        </NavigationNativeContainer>
+    );
 
     expect(comp.toJSON()).toMatchSnapshot();
 });
