8473e20f42dc7df95f8358126560c013bf80aedf
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2019-12-10 14:16:04 -0500

    chore: update publish webhook description

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -8,10 +8,12 @@ CHANNEL=$([ $IS_STABLE = "true" ] && echo "stable" || echo "staging")
 echo Publishing to $CHANNEL
 $(yarn global bin)/expo publish --non-interactive --release-channel $CHANNEL
 
+
+# TODO: ADD CHANGELOG
 WEBHOOK_DATA="{
    \"embeds\": [{
        \"title\": \"New $CHANNEL version published\",
-       \"description\": \"$COMMIT_MESSAGE\",
+       \"description\": \"The update will automatically download in the background while you use the app and will be avaliable the next time the app is opened\", 
        \"url\": \"https://exp.host/@dusterthefirst/WHS?release-channel=$CHANNEL\",
        \"color\": 32768,
        \"timestamp\": \"$(date -Iseconds -u)\",
