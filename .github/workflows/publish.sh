a03ec179db34170051b53e6d7ac7ea8719fa9305
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-01-14 08:58:57 -0500

    fix: remove flicker from loading screen and set expo-cli as local dep

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -1,12 +1,12 @@
 set -e
 
 echo Logging into Expo
-$(yarn global bin)/expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
+yarn expo login --non-interactive -u $EXPO_USERNAME -p $EXPO_PASSWORD
 
 CHANNEL=$([ $IS_STABLE = "true" ] && echo "stable" || echo "staging")
 
 echo Publishing to $CHANNEL
-$(yarn global bin)/expo publish --non-interactive --release-channel $CHANNEL
+yarn expo publish --non-interactive --release-channel $CHANNEL
 
 
 # TODO: ADD CHANGELOG
