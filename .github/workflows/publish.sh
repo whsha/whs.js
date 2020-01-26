d6765188d01b075e3e91ce621fad212c3914d356
DusterTheFirst <14093962+DusterTheFirst@users.noreply.github.com>
2020-01-26 14:15:50 -0500

    chore(publish): remove description from embed

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -12,7 +12,6 @@ yarn expo publish --non-interactive --release-channel $CHANNEL
 WEBHOOK_DATA="{
    \"embeds\": [{
        \"title\": \"New $CHANNEL version published\",
-       \"description\": \"The update will automatically download in the background while you use the app and will be avaliable the next time the app is opened\", 
        \"url\": \"https://exp.host/@dusterthefirst/WHS?release-channel=$CHANNEL\",
        \"color\": 32768,
        \"timestamp\": \"$(date -Iseconds -u)\",
